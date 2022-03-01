66997902de0f6a01701465ef66e366e893d1f0ea
Zach <shaw.zacht@gmail.com>
2022-03-01 15:36:17 -0800

    commit tree 171759e851cc6ab944a883667b3c5fe13c4041dc

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Your code here
+const dodger = document.querySelector('#dodger')
+
+document.addEventListener('keydown', function(event) {
+    if (event.key === 'ArrowLeft') {
+        moveDodgerLeft();
+    }
+})
+        
+function moveDodgerLeft() {
+    const leftNumbers = dodger.style.left.replace('px', '')
+    const left = parseInt(leftNumbers, 10)
+    
+    if (left > 0) {
+        dodger.style.left = `${left - 1}px`;
+    }
+}
+
+document.addEventListener('keydown', function(e) {
+    if (e.key === 'ArrowRight') {
+        moveDodgerRight();
+    }
+})
+
+function moveDodgerRight() {
+    const leftNumbers = dodger.style.left.replace('px', '');
+    const left = parseInt(leftNumbers, 10);
+
+    if (left < 360) {
+        dodger.style.left = `${left + 1}px`;
+    }
+}
